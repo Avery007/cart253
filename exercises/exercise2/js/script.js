043a8e7ab19f142ebfbc20a003aa8685c04375ad
Avery <veleda94@hotmail.com>
2019-09-19 21:00:59 -0400

    exercise2
    Ex2: Started exercise 2

---
+++ exercises/exercise2/js/script.js
@@ -0,0 +1,197 @@
+/******************************************************
+
+Game - The Artful Dodger
+Pippin Barr
+
+A simple dodging game with keyboard controls
+
+******************************************************/
+
+// The position and size of our avatar circle
+let avatarX;
+let avatarY;
+let avatarSize = 20;
+
+let word;
+let lose=0;
+// The speed and velocity of our avatar circle
+let avatarSpeed = 10;
+let avatarVX = 0;
+let avatarVY = 0;
+
+// The position and size of the enemy circle
+let enemyX;
+let enemyY;
+let enemySize = 5;
+
+// The speed and velocity of our enemy circle
+let enemySpeed = 5;
+let enemyVX = 5;
+
+// How many dodges the player has made
+let dodges = 0;
+
+let apple;
+let appleX;
+let appleY=0;
+let instruction;
+
+let player;
+let imgapple;
+
+// setup()
+//
+// Make the canvas, position the avatar and anemy
+function RandomN(low,high) {
+	return Math.floor(Math.random()*(high-low+1)+low);
+}
+
+function setup() {
+  // Create our playing area
+  createCanvas(1000,500);
+	player = loadImage("assets/images/run.png");
+	imgapple = loadImage("assets/images/apple.png");
+
+
+instruction="eat the apple to increase life spin!";
+  // Put the avatar in the centre
+  avatarX = width/2;
+  avatarY = height/2;
+
+  // Put the enemy to the left at a random y coordinate within the canvas
+  enemyX = 0;
+  enemyY = random(0,height);
+  appleX=random(10,width-10);
+  // No stroke so it looks cleaner
+  noStroke();
+}
+
+// draw()
+//
+// Handle moving the avatar and enemy and checking for dodges and
+// game over situations.
+function draw() {
+  // A pink background
+  background(255,220,220);
+
+  // Default the avatar's velocity to 0 in case no key is pressed this frame
+  avatarVX = 0;
+  avatarVY = 0;
+
+  // Check which keys are down and set the avatar's velocity based on its
+  // speed appropriately
+
+image(player,avatarX,avatarY);
+  // Left and right
+  if (keyIsDown(LEFT_ARROW)) {
+    avatarVX = -avatarSpeed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    avatarVX = avatarSpeed;
+  }
+
+  // Up and down (separate if-statements so you can move vertically and
+  // horizontally at the same time)
+  if (keyIsDown(UP_ARROW)) {
+    avatarVY = -avatarSpeed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    avatarVY = avatarSpeed;
+  }
+
+  // Move the avatar according to its calculated velocity
+  avatarX = avatarX + avatarVX;
+  avatarY = avatarY + avatarVY;
+
+  // The enemy always moves at enemySpeed
+  enemyVX = enemySpeed;
+  // Update the enemy's position based on its velocity
+
+  enemyX = enemyX + enemyVX;
+
+  // Check if the enemy and avatar overlap - if they do the player loses
+  // We do this by checking if the distance between the centre of the enemy
+  // and the centre of the avatar is less that their combined radii
+  if (dist(enemyX,enemyY,avatarX,avatarY) < enemySize/2+avatarSize/2) {
+    // Tell the player they lost
+    console.log("YOU LOSE!");
+    // Reset the enemy's position
+    lose=lose+1;
+    enemyX = 0;
+    enemyY = random(0,height);
+    // Reset the avatar's position
+    avatarX = width/2;
+    avatarY = height/2;
+    // Reset the dodge counter
+
+  }
+
+  // Check if the avatar has gone off the screen (cheating!)
+  if (avatarX < 0 || avatarX > width || avatarY < 0 || avatarY > height) {
+    // If they went off the screen they lose in the same way as above.
+    console.log("YOU LOSE!");
+    word="Opps! You fall down from the edge! move back to continue";
+    text(word,450,250);
+    enemySpeed = 0;
+    enemyX = 0;
+    dodges = 0;
+  }
+
+  // Check if the enemy has moved all the way across the screen
+  if (enemyX > width) {
+    // This means the player dodged so update its dodge statistic
+    dodges = dodges + 1;
+    // Tell them how many dodges they have made
+    console.log(dodges + " DODGES!");
+    // Reset the enemy's position to the left at a random height
+    enemyX = 0;
+    enemyY = random(0,height);
+  }
+
+  // Display the number of successful dodges in the console
+  console.log(dodges);
+
+  // The player is black
+  fill(0);
+  // Draw the player as a circle
+  ellipse(avatarX,avatarY,avatarSize,avatarSize);
+
+  // The enemy is red
+  fill(255,0,0);
+  // Draw the enemy as a rectangle
+    rectMode(CENTER);
+  rect(enemyX,enemyY,enemySize,enemySize);
+  enemySize=5+0.5*dodges;
+  enemySpeed=enemySize+0.5*dodges;
+
+  text(word,20,20);
+  text(lose,300,20);
+
+  avatarSize=20+lose*lose*5;
+
+  if(avatarSize> 150){
+    word="You lose!,your final dodges is "+ dodges;
+    text(word,20,20);
+    enemySpeed=0;
+    appleX=0;appleY=0;
+
+  }
+  else{  word="Your dodges" + dodges;}
+
+
+
+if(lose>=1){
+   appleY=appleY+1;
+   fill(255,0,0);
+  ellipse(appleX,appleY,25,25);
+  text(appleY,50,50);
+
+	text(instruction,400,300);
+}
+if (dist(appleX,appleY,avatarX,avatarY) < enemySize/2+avatarSize/2) {
+  lose=lose-1;
+  appleY=0;appleX=random(10,width-10);
+	instruction="";
+  }
+
+}
