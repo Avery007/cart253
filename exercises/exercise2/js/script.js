6b0eafb4f0bcb9aca394c0ba99b414dafb2b4150
Avery <veleda94@hotmail.com>
2019-09-22 16:54:10 -0400

    Update script.js

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -130,7 +130,7 @@ console.log(avatarSize);
   fill(255,0,0);
   rectMode(CENTER);
 // change the size and speed of enermy when players make dodges
-  if(enemySpeed<=45){           // set the max size of enermy to 40
+  if(enemySpeed<=45){           // set the max speed of enermy to 45
     	enemySpeed=10+0.5*dodges;  // rate of increased speed and size
       enemySize=enemySpeed/2;
       }
