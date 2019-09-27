2bacb57fa10d4604ace826d65763ae781d47cda8
Avery <veleda94@hotmail.com>
2019-09-27 20:47:59 +0100

    make game more difficult after each success

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -38,15 +38,18 @@ let opacity;
 
 // The number of decoys to show on the screen, randomly
 // chosen from the decoy images
-let numDecoys = 100;
+let numDecoys;
 
 // Keep track of whether they've won
 let gameOver = false;
+let gamecount;
+let nextgame;
 
 
-let final="YOU WINNED!";
-let textX;
-let textY=200;
+let restart="restart";
+let restartX;
+let restartY;
+
 
 // preload()
 //
@@ -72,10 +75,13 @@ function preload() {
 // of decoys in random positions, then the target
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  numDecoys=50;
   opacity=0;
+  gamecount=0;
   background(255,255,0,50);
   imageMode(CENTER);
-
+  restartX=0;
+  restartY=0;
   showtarget= targetImage;
   // Use a for loop to draw as many decoys as we need
   for (let i = 0; i < numDecoys; i++) {
@@ -134,7 +140,7 @@ rectMode(CENTER);
 rect(width-50,showY,100,100);
 
 
-let textX=0;
+
 
 
 }
@@ -156,32 +162,43 @@ function draw() {
   if (gameOver) {
     // Prepare our typography
     textFont("Helvetica");
-    textSize(128);
+    textSize(28);
     textAlign(CENTER,CENTER);
     noStroke();
     fill(random(255));
-
+     text("Good job!You got it!",width/2,height/2);
     // Tell them they won!
 
-
+     restartX=width/2;
+     restartY=height/2;
     // Draw a circle around the sausage dog to show where it is (even though
     // they already know because they found it!)
-    noFill();
-    stroke(random(255));
-    strokeWeight(10);
 
-    if (opacity<100){opacity=opacity+0.1;}
+    //strokeWeight(10);
 
-    if( opacity>50&&textX<windowWidth){  text(final,textX,textY);
-      textX=textX+1;
+    if (opacity<80){opacity=opacity+0.1;}
 
+    if( opacity>30&&targetX<windowWidth){
+      targetX=targetX+1;
+      image(targetImage,targetX,targetY);
+      fill(255);
+      text(restart,restartX,restartY);
 
-  }
-  else if(opacity<=49){ ellipse(targetX,targetY,targetImage.width,targetImage.height);}
-  else {textX=50;}
+       }
+  else if(opacity<=29){
+  noFill();
+  stroke(random(255));
+   ellipse(targetX,targetY,targetImage.width,targetImage.height);
+ }
+  else {targetX=50;}
 
 
 }
+
+else if(!gameOver){opacity=0;}
+
+
+
 }
 // mousePressed()
 //
@@ -198,4 +215,79 @@ function mousePressed() {
       gameOver = true;
     }
   }
+
+  else if (restartX!==0 && mouseX>restartX-50 && mouseX<restartX+50){
+    if (mouseY<restartY+30&& mouseY>restartY-30)     {
+       gameOver = false; restartX=20;
+//gamecount=gamecount+1;
+//numDecoys= numDecoys*gamecount*10;
+opacity=0;background(255,255,0,50); gamecount=gamecount+1;
+console.log(gamecount);
+newgame();
+    }
+  }
+
 }
+
+function newgame(){
+  numDecoys=numDecoys+gamecount*10;
+  console.log(numDecoys);
+   for (let i = 0; i < numDecoys; i++) {
+  // Choose a random location on the canvas for this decoy
+  let x = random(0,width);
+  let y = random(0,height);
+  // Generate a random number we can use for probability
+  let r = random();
+  // Use the random number to display one of the ten decoy
+  // images, each with a 10% chance of being shown
+  // We'll talk more about this nice quality of random soon enough.
+  // But basically each "if" and "else if" has a 10% chance of being true
+  if (r < 0.1) {
+    image(decoyImage1,x,y);
+  }
+  else if (r < 0.2) {
+    image(decoyImage2,x,y);
+  }
+  else if (r < 0.3) {
+    image(decoyImage3,x,y);
+  }
+  else if (r < 0.4) {
+    image(decoyImage4,x,y);
+  }
+  else if (r < 0.5) {
+    image(decoyImage5,x,y);
+  }
+  else if (r < 0.6) {
+    image(decoyImage6,x,y);
+  }
+  else if (r < 0.7) {
+    image(decoyImage7,x,y);
+  }
+  else if (r < 0.8) {
+    image(decoyImage8,x,y);
+  }
+  else if (r < 0.9) {
+    image(decoyImage9,x,y);
+  }
+  else if (r < 1.0) {
+    image(decoyImage10,x,y);
+  }
+}
+redraw();
+
+
+}
+
+
+//function newgame(){
+//  if(nextgame){ gamecount=gamecount+1; numDecoys= numDecoys*gamecount*10;opacity=0;
+
+//}
+
+
+
+  //else if ( restartX!==0 && mouseX > restartX-restart.width/2 && mouseX < restartX + restart.width/2) {
+    // Check if the cursor is also in the y range of the target
+    // i.e. check if it's within the top and bottom of the image
+  //  if (mouseY > restartY - restart.height/2 && mouseY < restarY + restart.height/2) {
+      //gameOver = false;
