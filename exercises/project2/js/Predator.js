5c1a3c2902b5f714dd96e9e2808f59c4036c5110
Avery <veleda94@hotmail.com>
2019-10-24 15:59:14 -0400

    P2: Started project 2 by using the final version of exercise 5

---
+++ exercises/project2/js/Predator.js
@@ -0,0 +1,197 @@
+// Predator
+//
+// A class that represents a simple predator
+// controlled by the arrow keys. It can move around
+// the screen and consume Prey objects to maintain its health.
+
+class Predator {
+
+  // constructor
+  //
+  // Sets the initial values for the Predator's properties
+  // Either sets default values or uses the arguments provided
+  constructor(x, y, speed, radius, player, img) {
+    // Position
+    this.x = x;
+    this.y = y;
+    // Velocity and speed
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = speed;
+    // Health properties
+    this.maxHealth = radius;
+    this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
+    this.healthLossPerMove = 0.05;
+    this.healthGainPerEat = 1;
+    // Display properties
+
+    this.radius = this.health; // Radius is defined in terms of health
+    // Input properties
+    this.p1upKey = UP_ARROW;
+    this.p1downKey = DOWN_ARROW;
+    this.p1leftKey = LEFT_ARROW;
+    this.p1rightKey = RIGHT_ARROW;
+
+    this.p2upKey = 87; // key W
+    this.p2downKey = 83; // key S
+    this.p2leftKey = 65; // key A
+    this.p2rightKey = 68; // key D
+
+    this.p1speedup = 13; // enter key
+    this.p2speedup = SHIFT;
+    this.playerNumber = player; // used to check which player
+
+    this.playerImg = img; // display player as images
+    this.isDead = false; // check if players are dead
+
+    this.eat = 0; // prey eaten number
+  }
+
+  // handleInput
+  //
+  // Checks if an arrow key is pressed and sets the predator's
+  // velocity appropriately.
+  handleInput() {
+    // Horizontal movement
+    if (this.playerNumber === 1) { // players as a tiger
+      if (keyIsDown(this.p1leftKey)) {
+        this.vx = -this.speed;
+      } else if (keyIsDown(this.p1rightKey)) {
+        this.vx = this.speed;
+      } else {
+        this.vx = 0;
+      }
+      // Vertical movement
+      if (keyIsDown(this.p1upKey)) {
+        this.vy = -this.speed;
+      } else if (keyIsDown(this.p1downKey)) {
+        this.vy = this.speed;
+      } else {
+        this.vy = 0;
+      }
+
+      if (keyIsDown(this.p1speedup)) { // speedup when shift is pressed
+        this.vy = this.vy * 3;
+        this.vx = this.vx * 3;
+
+      }
+    }
+
+    if (this.playerNumber === 2) { // player as eagle
+      if (keyIsDown(this.p2leftKey)) {
+        this.vx = -this.speed;
+      } else if (keyIsDown(this.p2rightKey)) {
+        this.vx = this.speed;
+      } else {
+        this.vx = 0;
+      }
+      // Vertical movement
+      if (keyIsDown(this.p2upKey)) {
+        this.vy = -this.speed;
+      } else if (keyIsDown(this.p2downKey)) {
+        this.vy = this.speed;
+      } else {
+        this.vy = 0;
+      }
+
+      if (keyIsDown(this.p2speedup)) { // speedup when enter key is pressed
+        this.vy = this.vy * 3;
+        this.vx = this.vx * 3;
+
+      }
+    }
+
+
+
+  }
+  // move
+  //
+  // Updates the position according to velocity
+  // Lowers health (as a cost of living)
+  // Handles wrapping
+  move() {
+    // Update position
+    this.x += this.vx;
+    this.y += this.vy;
+    // Update health
+    if (this.isDead) // rest player health and size when it is dead
+    {
+      this.health = 0;
+      this.radius = 0;
+    } else { // reudce play health when game is active
+      this.health = this.health - this.healthLossPerMove;
+      this.health = constrain(this.health, 0, this.maxHealth);
+      // Handle wrapping
+      this.handleWrapping();
+    }
+  }
+
+  // handleWrapping
+  //
+  // Checks if the predator has gone off the canvas and
+  // wraps it to the other side if so
+  handleWrapping() {
+    // Off the left or right
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+    // Off the top or bottom
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+  // handleEating
+  //
+  // Takes a Prey object as an argument and checks if the predator
+  // overlaps it. If so, reduces the prey's health and increases
+  // the predator's. If the prey dies, it gets reset.
+  handleEating(prey) {
+    // Calculate distance from this predator to the prey
+    let d = dist(this.x, this.y, prey.x, prey.y);
+    // Check if the distance is less than their two radii (an overlap)
+    if (d < this.radius + prey.radius) {
+      // Increase predator health and constrain it to its possible range
+      this.health += this.healthGainPerEat;
+      this.health = constrain(this.health, 0, this.maxHealth);
+
+      // Decrease prey health by the same amount
+      prey.health -= this.healthGainPerEat;
+
+      // Check if the prey died and reset it if so
+      if (prey.health < 0) {
+        this.eat = this.eat + 1; // track how mnay preys the player eat
+        prey.reset();
+
+      }
+    }
+  }
+
+  // chekc if player is dead
+  checkState() {
+
+    if (this.health < 1) {
+      this.isDead = true;
+    }
+  }
+
+  // display
+  //
+  // Draw the predator as an ellipse on the canvas
+  // with a radius the same size as its current health.
+  display() {
+    if (!this.isDead) { // display when game is actvie
+      push();
+      noStroke();
+      imageMode(CENTER);
+      this.radius = this.health;
+      image(this.playerImg, this.x, this.y, 2 * this.radius, 2 * this.radius);
+      console.log(this.radius);
+      pop();
+    }
+  }
+}
---
+++ exercises/project2/js/script.js
@@ -0,0 +1,129 @@
+// Predator-Prey Simulation
+// by Qingyi Deng
+//
+// two players play as predators：eagle and tiger, with different key controls
+//and three prey (of different sizes and speeds)
+// The predator should chase the prey and consumes them.
+// The predator loses health over time, so must keep eating to survive.
+// The winter is the one who eat most preys
+
+// Our predator
+let tiger;
+let eagle;
+// The three prey
+let antelope;
+let zebra;
+let rabbit;
+
+// display how many preys eaten
+let player1Info;
+let player2Info;
+
+// display players and background by image
+let tigerImg; //tiger image
+let eagleImg; // eagle image
+let background; // background img
+let front; // front image
+
+// add background musci
+let backMusic;
+
+let result = 0; // tracking game state
+
+// setup()
+//
+// Sets up a canvas
+// Creates objects for the predator and three prey
+
+function preload() {
+
+  tigerImg = loadImage('./assets/images/tiger.png');
+  eagleImg = loadImage('./assets/images/eagle.png');
+  background = loadImage('./assets/images/forest.jpg');
+  front = loadImage('./assets/images/front.png');
+  backMusic = loadSound('./assets/sounds/music.wav');
+}
+
+// function set up
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  tiger = new Predator(100, 100, 5, 40, 1, tigerImg); // source pixably
+  eagle = new Predator(200, 200, 5, 40, 2, eagleImg); // source pixably
+  antelope = new Prey(100, 100, 10, color(255, 100, 10), 20, "antelope");
+  zebra = new Prey(100, 100, 8, color(255, 255, 255), 25, "zebra");
+  rabbit = new Prey(100, 100, 20, color(255, 255, 0), 10, "rabbit");
+}
+
+// draw()
+//
+// Handles input, movement, eating, and displaying for the system's objects
+function draw() {
+
+  if (result === 0) { // before game starts, display the front image
+
+    image(front, 0, 0, width, height);
+  }
+  // when game starts
+  else if (result === 1) {
+    image(background, 0, 0, windowWidth, windowHeight); // display background
+    instruction(); // show player's information
+    gameOver(); //check if game over and display text when it is true
+    // Handle input for the tiger
+    tiger.handleInput();
+    eagle.handleInput();
+
+    // Move all the "animals"
+    tiger.move();
+    eagle.move();
+    antelope.move();
+    zebra.move();
+    rabbit.move();
+
+    // tracking if predators are dead
+    tiger.checkState();
+    eagle.checkState();
+
+    // Handle the tiger eating any of the prey
+    tiger.handleEating(antelope);
+    tiger.handleEating(zebra);
+    tiger.handleEating(rabbit);
+
+    eagle.handleEating(antelope);
+    eagle.handleEating(zebra);
+    eagle.handleEating(rabbit);
+
+    // Display all the "animals"
+    tiger.display();
+    eagle.display();
+    antelope.display();
+    zebra.display();
+    rabbit.display();
+  }
+
+}
+
+function instruction() {
+  // show how many preys players eat
+  player1Info = "tiger eats:" + tiger.eat;
+  player2Info = "eagle eats:" + eagle.eat;
+  textSize(20);
+  fill(255);
+  text(player1Info, windowWidth / 2, 70);
+  text(player2Info, windowWidth / 2, 100);
+
+}
+
+function gameOver() {
+  if (tiger.isDead && eagle.isDead) { // check if both of the predators are dead,if so, game over
+    result === 2; // change game state , game over
+    text("Game over ! now you know who ate more !", windowWidth / 2, 150); // display when game over
+  }
+}
+
+function mousePressed() {
+  if (result === 0) {
+    result = 1; // start the game when mouse is clicked
+    backMusic.loop(); // play music when game starts
+  }
+}
