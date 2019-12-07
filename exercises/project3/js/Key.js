7ff4540a900c6a53c8475c7661ead976e4d4c697
Avery <veleda94@hotmail.com>
2019-12-07 12:41:52 -0500

    P3:Added a new class Key

---
+++ exercises/project3/js/Key.js
@@ -0,0 +1,100 @@
+class Key { //set key properties
+
+  //construction
+  constructor(size, speed, img) {
+    // random position
+    this.x = random(0 width);
+    this.y = random(0, height);
+    // Velocity and speed
+    this.vx = 0;
+    this.vy = 0;
+
+    this.originalSpeed = speed;
+    this.speed = this.originalSpeed;
+    // Time properties for noise() function
+    this.tx = random(500, 1000); // To make x and y noise different
+    this.ty = random(0, 1000); // we use random starting values
+    // power properties
+
+
+    this.size = size;
+
+     this.isGot=false;
+
+    this.img = img;
+
+
+
+  }
+
+  movement() {
+    if (!this.isGot) {// when elites is active
+      // Set velocity via noise()
+      this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
+      this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
+      // Update position
+      this.x += this.vx;
+      this.y += this.vy;
+      // Update time properties
+      this.tx += 0.01;
+      this.ty += 0.01;
+      // Handle wrapping
+      this.handleWrapping();
+    }
+
+  }
+
+  handleWrapping() {
+    // Off the left or right
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+    // Off the top or bottom
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+
+
+  display() {
+
+
+      push();
+      imageMode(CENTER);
+      image(this.img, this.x, this.y, this.size/2, this.size);
+
+      pop();
+      if(!this.isGot){
+        fill(random(0,255));
+        textSize(15);
+        text("Got me",this.x,this.y-this.size);
+
+
+      }
+    }
+
+
+
+
+   GotKey(cathari) {
+      // Calculate distance from this cadidates
+      let d = dist(this.x, this.y, cathari.x, cathari.y);
+
+      // Check if the distance is less than their two radii (an overlap)
+      if (d < this.size/2 + cathari.size/2) {
+        if(!cathari.shieldActive){
+        // Increase candidates power and constrain it to its possible range
+        this.isGot=true;
+
+
+}
+      }
+    }
+
+
+}
